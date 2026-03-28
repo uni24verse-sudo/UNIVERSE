ca93a2c6456be087c5ed1471b7bfa1d86fea5ab0
ParthSharma2404 <parthsharma240404@gmail.com>
2026-03-28 21:23:47 +0530

    fix: remove legacy UPI route causing MODULE_NOT_FOUND crash

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,6 @@ app.use('/api/store', require('./routes/store'));
 app.use('/api/orders', require('./routes/orders'));
 app.use('/api/scan-menu', require('./routes/menuScanner'));
 app.use('/api/super-admin', require('./routes/superAdmin'));
-app.use('/api/upi', require('./routes/upi'));
 app.use('/api/payments', require('./routes/payments'));
 
 // Socket.io connection handling
