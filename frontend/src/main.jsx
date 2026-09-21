550832521f2c33d843d97c97909f49eae389cc1d
uni24verse-sudo <uni24verse@gmail.com>
2026-09-21 23:46:39 +0530

    feat(ci-cd): add automated GitHub Actions workflows and harden origin resolver

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,15 +6,17 @@ import NotificationManager from './utils/notifications.js'
 import axios from 'axios'
 
 // Universal API URL Resolver:
-// Intercepts all requests that attempt to go to http://localhost:5000 when deployed on HTTPS/cloud
+// Intercepts all requests that attempt to go to http://localhost:5000 or broken api domains when deployed on HTTPS/cloud
 axios.interceptors.request.use((config) => {
   if (typeof window !== 'undefined' && window.location.hostname && 
       window.location.hostname !== 'localhost' && 
       window.location.hostname !== '127.0.0.1') {
-    if (config.url && config.url.startsWith('http://localhost:5000')) {
-      config.url = config.url.replace('http://localhost:5000', window.location.origin);
-    } else if (config.url && config.url.startsWith('http://')) {
-      config.url = config.url.replace(/^http:\/\//, 'https://');
+    if (config.url) {
+      if (config.url.startsWith('http://localhost:5000') || config.url.includes('api.universeorder.co.in')) {
+        config.url = config.url.replace(/^(https?:\/\/api\.universeorder\.co\.in|http:\/\/localhost:5000)/, window.location.origin);
+      } else if (config.url.startsWith('http://')) {
+        config.url = config.url.replace(/^http:\/\//, 'https://');
+      }
     }
   }
   return config;
